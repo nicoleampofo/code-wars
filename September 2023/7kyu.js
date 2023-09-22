a660d79d4f82e052f3de93fd74ea49313c53fade
nicoleampofo <nicabuca@gmail.com>
2023-09-21 20:07:52 -0400

    codewars 9-21-2023

--- September 2023/7kyu.js
+++ September 2023/7kyu.js
@@ -15,3 +15,22 @@ function sumOfTwoLowest(arr){
 }
 
 sumOfTwoLowest([22, 8, 5, 19, 12])
+
+
+
+
+// This function should test if the factor is a factor of base.
+
+// Return true if it is a factor or false if it is not.
+
+function checkForFactor (base, factor) {
+    if (base % factor == 0){
+          return true
+      } else {
+          return false
+      }
+  }
+
+
+  checkForFactor(2, 6)
+  // should return True
